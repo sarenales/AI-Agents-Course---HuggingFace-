ecec917ec5225da10564de6c8c35d40937bae65e
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2025-09-05 08:29:37 +0200

    my first rag with langchain

---
+++ rag/main.py
@@ -0,0 +1,26 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama2")
+
+template = """
+    You are an expert in answering questions about a pizza restaurant.
+
+    Here are some relevant reviews: {reviews}
+
+    Here is the question to answer: {question}
+
+    """
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n---------------------------------------")
+    question = input("Ask your question (q to quit):")
+    print("\n\n")
+    if question == "q":
+        break
+    reviews = retriever.invoke(question)
+    response = chain.invoke({"reviews": reviews, "question" : question})
+    print(response)
